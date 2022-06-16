d4e7803014eefb69a626a2e743ecf0b420ce269d
Ben Lieberman <bhlieberman93@gmail.com>
2022-06-16 19:21:34 -0400

    CSS added

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { 
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+} from './layout.module.css'
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ pageTitle, children }): JSX.Element => {
   return (
-    <div>
+    <div className={container}>
       <title>{pageTitle}</title>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+        <ul className={navLinks}>
+          <li><Link to="/" className={navLinkItem}>
+            Home
+            </Link>
+          </li>
+          <li><Link to="/about" className={navLinkItem}>
+            About
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
     </div>
