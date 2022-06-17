0752258b05d7d2f6deb0ec2df39d9456c6b4508f
Ben Lieberman <bhlieberman93@gmail.com>
2022-06-17 12:35:08 -0400

    added blog posts and MDX rendering

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
@@ -7,23 +8,32 @@ const BlogPage = ({ data }) => {
         <Layout pageTitle="My Blog Posts">
             <ul>
                 {
-                    data.allFile.nodes.map(node => {
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    })
+                    data.allMdx.nodes.map((node) => (
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
+                    ))
                 }
             </ul>
         </Layout>
     )
 }
 
-const query = graphql`
+export const query = graphql`
   query {
-    allFile {
-        nodes {
-            name
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        body
+      }
     }
   }
 `
